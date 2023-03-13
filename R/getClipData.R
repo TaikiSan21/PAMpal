f745a844b6948bad89834f65baa0e99fc6d111a4
Taiki <tnsakai@gmail.com>
2023-03-13 13:05:28 -0700

    smol fix for no err

--- R/getClipData.R
+++ R/getClipData.R
@@ -91,8 +91,9 @@ getClipData <- function(x, buffer = c(0, 0.1), mode=c('event', 'detection'),
     })
     # one DB at a time
     for(d in seq_along(dbMap)) {
-        thisDbData <- x[which(evDbs == names(dbMap)[d])]
-        if(length(events(thisDbData)) == 0) next
+        thisDbMatch <- which(evDbs == names(dbMap)[d])
+        if(length(thisDbMatch) == 0) next
+        thisDbData <- x[thisDbMatch]
         wavMap <- dbMap[[d]]
         allTimes <- getTimeRange(thisDbData, mode=mode, sample=useSample)
         allResult <- vector('list', length = length(allTimes))
